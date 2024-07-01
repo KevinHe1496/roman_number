cb3b694b95c9cdc9adb8c80220d7260f496129b1
Kevin Heredia <kevin@ravecodesolutions.com>
2024-07-01 15:13:07 -0500

    Paso test de 1 al 9 y 1 al 1000

--- roman_funcs.py
+++ roman_funcs.py
@@ -10,6 +10,17 @@ valors = {
 
 def to_roman(n: int) -> str:
 
-    result = valors[n]
+    result = ''
+
+    if n <= 3:
+     result = n * valors[1]
+    elif n == 4:
+       result = valors[1] + valors[5]
+    elif n < 9:
+       result = valors[5] + (n - 5) * valors[1]
+    elif n == 9:
+       result = valors[1] + valors[10]
+    else:
+       result = valors[n]
     return result
  
