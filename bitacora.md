76e197f7a1cfbcf00c7b59a20f27ce6f08cf4f3e
Kevin Heredia <kevin@ravecodesolutions.com>
2024-07-01 14:01:13 -0500

    Tests: con diccionarios

---
+++ bitacora.md
@@ -0,0 +1,9 @@
+# Tests
+
+- Crear una función que pase de numero arabigo a romano (menor de 4000)
+```python
+to_roman(n: int) -> str
+```
+
+- 1 -> "I"
+- 5 -> "V"
---
+++ roman_funcs.py
@@ -0,0 +1,15 @@
+valors = {
+    1: "I",
+    5: "V",
+    10: "X",
+    50: "L",
+    100: "C",
+    500: "D",
+    1000: "M"
+}
+
+def to_roman(n: int) -> str:
+
+    result = valors[n]
+    return result
+ 
